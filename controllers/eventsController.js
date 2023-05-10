7e8895b45ef747a46edf0590740c42fb8f5a0f53
MohamedAdel310 <99290531+MohamedAdelElBaik@users.noreply.github.com>
2023-05-10 15:44:55 +0300

    add two functions to not repeat code

--- controllers/eventsController.js
+++ controllers/eventsController.js
@@ -38,18 +38,7 @@ exports.addEvent = async (req, res) => {
   }
 };
 
-exports.getEventsForYear = catchAsync(async (req, res, next) => {
-  const year = req.params.year;
-
-  const startDate = new Date(`${year}-01-01T00:00:00.000Z`);
-  const endDate = new Date(`${year}-12-31T23:59:59.999Z`);
-
-  const query = {
-    arriveAt: { $gte: startDate, $lt: endDate },
-  };
-
-  const events = await Event.find(query);
-
+const getEventRequest = (events, res) => {
   const countObj = {};
   events.forEach((event) => {
     countObj[event.type] = (countObj[event.type] || 0) + 1;
@@ -61,57 +50,39 @@ exports.getEventsForYear = catchAsync(async (req, res, next) => {
     types: countObj,
     data: events,
   });
+};
+
+const getDate = (params) => {
+  const year = params.year;
+  const month = params.month;
+  const day = params.day;
+
+  const startDate = new Date(
+    `${year}-${month || '01'}-${day || '01'}T00:00:00.000Z`
+  );
+  const endDate = new Date(
+    `${year}-${month || '12'}-${day || '31'}T23:59:59.999Z`
+  );
+
+  return {
+    arriveAt: { $gte: startDate, $lt: endDate },
+  };
+};
+
+exports.getEventsForYear = catchAsync(async (req, res, next) => {
+  const events = await Event.find(getDate(req.params));
+
+  getEventRequest(events, res);
 });
 
 exports.getEventsForMonth = catchAsync(async (req, res, next) => {
-  const year = req.params.year;
-  const month = req.params.month;
+  const events = await Event.find(getDate(req.params));
 
-  const startDate = new Date(`${year}-${month}-01T00:00:00.000Z`);
-  const endDate = new Date(`${year}-${month}-31T23:59:59.999Z`);
-
-  const query = {
-    arriveAt: { $gte: startDate, $lt: endDate },
-  };
-
-  const events = await Event.find(query);
-
-  const countObj = {};
-  events.forEach((event) => {
-    countObj[event.type] = (countObj[event.type] || 0) + 1;
-  });
-
-  res.status(200).json({
-    status: 'success',
-    results: events.length,
-    types: countObj,
-    data: events,
-  });
+  getEventRequest(events, res);
 });
 
 exports.getEventsForDay = catchAsync(async (req, res, next) => {
-  const year = req.params.year;
-  const month = req.params.month;
-  const day = req.params.day;
+  const events = await Event.find(getDate(req.params));
 
-  const startDate = new Date(`${year}-${month}-${day}T00:00:00.000Z`);
-  const endDate = new Date(`${year}-${month}-${day}T23:59:59.999Z`);
-
-  const query = {
-    arriveAt: { $gte: startDate, $lt: endDate },
-  };
-
-  const events = await Event.find(query);
-
-  const countObj = {};
-  events.forEach((event) => {
-    countObj[event.type] = (countObj[event.type] || 0) + 1;
-  });
-
-  res.status(200).json({
-    status: 'success',
-    results: events.length,
-    types: countObj,
-    data: events,
-  });
+  getEventRequest(events, res);
 });
